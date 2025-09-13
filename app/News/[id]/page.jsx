df12fe49d7d27f482f01a4880024bb231140530b
Max <knyaz010official@gmail.com>
2025-09-13 09:16:13 +0300

    create dynamic metadata

--- app/News/[id]/page.jsx
+++ app/News/[id]/page.jsx
@@ -1,11 +1,41 @@
 // app/news/[id]/page.jsx
 import './NewsItem.scss';
 import Link from 'next/link';
-import newsArr from '@/app/News/data';
 import Header from "@/layouts/Header/Header";
 import Footer from "@/layouts/Footer/Footer";
 import './NewsItem.scss'
 import Button from "@/components/Button/Button";
+
+import newsArr from "@/app/News/data";
+
+export async function generateMetadata({ params }) {
+    const news = newsArr.find((n) => n.id === params.id);
+
+    if (!news) {
+        return {
+            title: 'Новость не найдена',
+            description: 'Страница не найдена',
+        }
+    }
+
+    return {
+        title: `${news.title} | Новости Усадьбы Комарово`,
+        description: news.fullText.slice(0, 160), // берём первые 160 символов
+        openGraph: {
+            title: news.title,
+            description: news.fullText.slice(0, 160),
+            images: [
+                {
+                    url: news.imgLink,
+                    width: 1200,
+                    height: 630,
+                    alt: news.title,
+                }
+            ],
+        },
+    }
+}
+
 
 export default function NewsItem({ params }) {
     const news = newsArr.find(n => n.id === params.id);
